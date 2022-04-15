560d218219c95b59561005ded39c2d076be37cf8
Nicaise Fofack <nfo@ibanfirst.com>
2022-04-15 15:48:01 +0200

    add setup for erpnext

---
+++ erpnext_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "starting frappe-erpnext docker stack configuration..."
+# Point to main compose file (compose.yaml) and add one more.
+docker-compose -f compose.yaml \
+ -f overrides/compose.mariadb.yaml \
+ -f overrides/compose.redis.yaml \
+ -f overrides/compose.proxy.yaml \
+ -f overrides/compose.erpnext.yaml \
+ config
